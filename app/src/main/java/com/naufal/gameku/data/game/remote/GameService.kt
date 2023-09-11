ad52b420af6685de15dd5b340a5a56be0e471aef
naufalrachmandani <naufrach@gmail.com>
2023-09-11 17:53:47 +0700

    + add domain layer

--- app/src/main/java/com/naufal/gameku/data/game/remote/GameService.kt
+++ app/src/main/java/com/naufal/gameku/data/game/remote/GameService.kt
@@ -10,7 +10,7 @@ import retrofit2.http.Query
 interface GameService {
     @GET("games")
     suspend fun getGames(
-        @Query("key") key: String = "a300591704b449bfad1ecf786507abcd",
+        @Query("key") key: String = "",
         @Query("page_size") pageSize: Int = 10,
         @Query("page") page: Int = 1,
         @Query("search") search: String = "",
@@ -19,6 +19,6 @@ interface GameService {
     @GET("games/{gameId}")
     suspend fun getGameDetail(
         @Path("gameId") gameId: Int?,
-        @Query("key") key: String = "a300591704b449bfad1ecf786507abcd",
+        @Query("key") key: String = "",
     ): Response<GameDetailResponse>
 }
